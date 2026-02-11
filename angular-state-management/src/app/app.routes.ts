0912b3a0c1f0927cfb52cdb45001a500cf483dc1
SelvamUST <selvakumar.m@ust.com>
2026-02-11 14:51:34 +0530

    Counter component added.

--- angular-state-management/src/app/app.routes.ts
+++ angular-state-management/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Routes } from '@angular/router';
 import { Products } from './components/products/products';
 import { Orders } from './components/orders/orders';
 import { Welcome } from './components/welcome/welcome';
+import {Counter } from './components/counter/counter';
 
 export const routes: Routes = [
   { path: '', component: Welcome },
   { path: 'products', component: Products },
   { path: 'orders', component: Orders }
+  { path: 'counter', component:Counter}
 ];
